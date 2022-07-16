47c71a586ca99165fb83c688545aa257e0b9e048
Kimani <elvis.kimani@student.moringaschool.com>
2022-07-16 19:00:58 +0300

    commit tree f7b894cd8b931294ae7a8d99a1f06213ec1790ec

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header ({onDarkModeClick, mode}) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+               {mode ? "Light" : "Dark"} Mode
+            </button>     
+      </header>
+
+    )
+}
+
+export default Header
